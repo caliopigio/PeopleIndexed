3c6291661e8b9f5a1cf4b18eeb03ba6526a9a5a2
Luis Carlos Larrañaga <caliopigio@gmail.com>
2011-09-29 21:56:28 -0500

    search bar added

--- Headers/PeopleViewController.h
+++ Headers/PeopleViewController.h
@@ -1,14 +1,22 @@
+#import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-@interface PeopleViewController : UITableViewController
+@interface PeopleViewController : UITableViewController <UISearchBarDelegate,
+        UISearchDisplayDelegate, UITableViewDataSource, UITableViewDelegate>
 {
     UILocalizedIndexedCollation *collation;
     NSArray *people;
     NSMutableArray *peopleSortedArray;
+    NSMutableArray *filteredArray;
+    UISearchDisplayController *searchDisplay;
+    UISearchBar *searchBar;
 }
 
 @property (nonatomic, retain) UILocalizedIndexedCollation *collation;
 @property (nonatomic, retain) NSArray *people;
 @property (nonatomic, retain) NSMutableArray *peopleSortedArray;
+@property (nonatomic, retain) NSMutableArray *filteredArray;
+@property (nonatomic, retain) UISearchDisplayController *searchDisplay;
+@property (nonatomic, retain) UISearchBar *searchBar;
 
 @end
--- Sources/PeopleViewController.m
+++ Sources/PeopleViewController.m
@@ -1,3 +1,6 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
 #import "PeopleViewController.h"
 
 #import "Person.h"
@@ -8,6 +11,9 @@
 @synthesize collation;
 @synthesize people;
 @synthesize peopleSortedArray;
+@synthesize filteredArray;
+@synthesize searchDisplay;
+@synthesize searchBar;
 
 #pragma mark -
 #pragma mark NSObject
@@ -24,9 +30,23 @@
 {
     [super viewDidLoad];
     
+    searchBar = [[UISearchBar alloc] initWithFrame:CGRectMake(0, 0, 320, 44)];
+    
+    [searchBar setDelegate:self];
+    [self.tableView setTableHeaderView:searchBar];
+    
+    searchDisplay = [[UISearchDisplayController alloc] initWithSearchBar:
+            searchBar contentsController:self];
+    
+    [searchBar release];
+    [searchDisplay setDelegate:self];
+    [searchDisplay setSearchResultsDelegate:self];
+    [searchDisplay setSearchResultsDataSource:self];
+    
     self.collation = [UILocalizedIndexedCollation currentCollation];
     NSMutableArray *newPeopleArray = [NSMutableArray arrayWithCapacity:
             [[self.collation sectionTitles] count]];
+    self.filteredArray = [NSMutableArray arrayWithCapacity:[self.people count]];
     NSInteger i;
     
     for (i = 0; i < [[self.collation sectionTitles] count]; i++) {
@@ -52,7 +72,7 @@
         [newPeopleArray replaceObjectAtIndex:i withObject:sortedPeopleArray]; 
     }
     
-    self.peopleSortedArray = newPeopleArray;
+    self.peopleSortedArray = newPeopleArray;    
 }
 
 #pragma mark - 
@@ -60,12 +80,18 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
+    if (tableView == self.searchDisplayController.searchResultsTableView) {
+        return 1;
+    }
     return  [self.peopleSortedArray count];
 }
 
 - (NSInteger)       tableView:(UITableView *)tableView 
         numberOfRowsInSection:(NSInteger)section
 {
+    if (tableView == self.searchDisplayController.searchResultsTableView) {
+        return [self.filteredArray count];
+    }
     return [[self.peopleSortedArray objectAtIndex:section] count];
 }
 
@@ -81,15 +107,22 @@
                 UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] 
                     autorelease];
     }
-    
-    cell.textLabel.text = [[[self.peopleSortedArray objectAtIndex:
-            indexPath.section] objectAtIndex:indexPath.row] name];
+    if (tableView == self.searchDisplayController.searchResultsTableView) {
+        cell.textLabel.text = [[self.filteredArray objectAtIndex:
+                indexPath.row] name];
+    } else {
+        cell.textLabel.text = [[[self.peopleSortedArray objectAtIndex:
+                indexPath.section] objectAtIndex:indexPath.row] name];
+    }
     return cell;
 }
 
 - (NSString *)      tableView:(UITableView *)tableView 
       titleForHeaderInSection:(NSInteger)section 
 {
+    if (tableView == self.searchDisplayController.searchResultsTableView) {
+        return nil;
+    }
     if ([[self.peopleSortedArray objectAtIndex:section] count] == 0) {
         return nil;
     }
@@ -100,11 +133,17 @@
   sectionForSectionIndexTitle:(NSString *)title 
                       atIndex:(NSInteger)index 
 {
+    if (tableView == self.searchDisplayController.searchResultsTableView) {
+        return 0;
+    }
     return [self.collation sectionForSectionIndexTitleAtIndex:index];
 }
 
 - (NSArray *)sectionIndexTitlesForTableView:(UITableView *)tableView 
 {
+    if (tableView == self.searchDisplayController.searchResultsTableView) {
+        return nil;
+    }
     return [self.collation sectionIndexTitles];
 }
 
@@ -116,6 +155,28 @@
   didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+#pragma mark -
+#pragma mark <UISearchDisplayDelegate>
+
+- (BOOL)        searchDisplayController:(UISearchDisplayController *)controller
+       shouldReloadTableForSearchString:(NSString *)searchString
+{
+    [self.filteredArray removeAllObjects];
+    
+    NSArray *peopleSorted = [self.collation sortedArrayFromArray:people 
+            collationStringSelector:@selector(name)];
+    
+    for (Person *person in peopleSorted) {
+        NSComparisonResult result = [person.name compare:searchString
+                options:(NSCaseInsensitiveSearch | NSDiacriticInsensitiveSearch) 
+                    range:NSMakeRange(0, [searchString length])];
+        if (result == NSOrderedSame) {
+            [self.filteredArray addObject:person];
+        }
+    }
+    return YES;
 }
 
 @end
